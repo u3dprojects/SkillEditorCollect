021c70d1bbc678e8fd298eb12240757194ff8e31
Canyon <379657505@qq.com>
2016-12-22 18:35:30 +0800

    将绘制抽出来独立成了一个类

---
+++ Assets/Scriptes/DBOpt/DBOpt_GUI.cs
@@ -0,0 +1,132 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+/// <summary>
+/// 类名 : animator的试图Inspector 中的操作
+/// 作者 : Canyon
+/// 日期 : 2016-12-22 17:30:00
+/// 功能 : 
+/// </summary>
+[System.Serializable]
+public class DBOpt_GUI : System.Object{
+
+    DBOpt_Ani db_opt_ani;
+    // DBOpt_Time db_opt_time = new DBOpt_Time();
+    
+    // 控制行间 - 间隔距离
+    float space_row_interval = 10.0f;
+    
+    // popup 列表选择值
+    int ind_popup = 0;
+    int pre_ind_popup = -1;
+
+    // 速度控制
+    float cur_speed = 1.0f;
+
+    float min_speed = 0.0f;
+    float max_speed = 3.0f;
+
+    public void DoClear()
+    {
+        db_opt_ani = null;
+    }
+
+    public void Reset()
+    {
+        pre_ind_popup = -1;
+        ind_popup = 0;
+
+        cur_speed = 1.0f;
+    }
+
+    public void DoInit(DBOpt_Ani db_ani)
+    {
+        Reset();
+        
+        this.db_opt_ani = db_ani;
+    }
+
+    public float CurSpeed
+    {
+        get { return cur_speed; }
+    }
+    
+    public void DrawRowLine()
+    {
+        EditorGUILayout.BeginHorizontal();
+        {
+            GUILayout.Label("行间间距:");
+            space_row_interval = EditorGUILayout.Slider(space_row_interval, 0, 100);
+        }
+        EditorGUILayout.EndHorizontal();
+
+        GUILayout.Space(space_row_interval);
+    }
+
+    public bool DrawJudged()
+    {
+        if (db_opt_ani == null)
+        {
+            EditorGUILayout.HelpBox("请将脚本绑定到一个带Animator的物体！", MessageType.Error);
+            return false;
+        }
+
+        if (db_opt_ani.Keys.Count <= 0)
+        {
+            EditorGUILayout.HelpBox("该AnimatorController里面没有任何动画，请添加动画！", MessageType.Error);
+            return false;
+        }
+        return true;
+    }
+
+    public void DrawRefreshAnimator()
+    {
+        if (GUILayout.Button("刷新Animator动画列表"))
+        {
+            DoInit(this.db_opt_ani);
+        }
+        GUILayout.Space(space_row_interval);
+    }
+
+    public void DrawAniListIndex()
+    {
+        ind_popup = EditorGUILayout.Popup("动画列表", ind_popup, db_opt_ani.Keys.ToArray());
+        if (pre_ind_popup != ind_popup)
+        {
+            pre_ind_popup = ind_popup;
+            db_opt_ani.ResetAniState(ind_popup);
+            db_opt_ani.SetSpeed(1.0f);
+
+            cur_speed = 1.0f;
+        }
+
+        GUILayout.Space(space_row_interval);
+    }
+
+    public void DrawAniInfo()
+    {
+        EditorGUILayout.BeginHorizontal();
+        {
+            GUILayout.Label("总帧数: " + db_opt_ani.CurFrameCount);
+
+            EditorGUILayout.LabelField("总时长: " + db_opt_ani.CurLens + " s");
+        }
+        EditorGUILayout.EndHorizontal();
+
+        GUILayout.Space(space_row_interval);
+    }
+
+    public void DrawSpeed()
+    {
+        EditorGUILayout.BeginHorizontal();
+        {
+            GUILayout.Label("速度:");
+            // cur_speed = GUILayout.HorizontalSlider(cur_speed,min_speed, max_speed);
+            cur_speed = EditorGUILayout.Slider(cur_speed, min_speed, max_speed);
+        }
+        EditorGUILayout.EndHorizontal();
+
+        GUILayout.Space(space_row_interval);
+    }
+}
---
+++ Assets/Scriptes/SkillEditor5/Editor/ED_Skill05_Inspector.cs
@@ -0,0 +1,195 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+/// <summary>
+/// 类名 : Demo04-进度
+/// 作者 : Canyon
+/// 日期 : 2016-12-22 17:20:00
+/// 功能 : 抽出绘制区域
+/// </summary>
+[CanEditMultipleObjects]
+[CustomEditor(typeof(ED_Skill05), true)]
+public class ED_Skill05_Inspector : Editor
+{
+    DBOpt_Ani db_opt_ani = new DBOpt_Ani();
+    DBOpt_Time db_opt_time = new DBOpt_Time();
+    DBOpt_GUI draw_gui = new DBOpt_GUI();
+    
+    ED_Skill05 m_entity;
+    Animator m_ani;
+
+    // 更新是否是通过EditorApplication的时间
+    bool isUpAniByEdTime = false;
+    
+    // 暂停按钮控制
+    bool isPauseing = false;
+
+    // 播放按钮的控制值
+    bool isPlaying = false;
+    
+
+    void OnEnable()
+    {
+        DoInit();
+        EditorApplication.update += OnUpdate;
+    }
+
+    void OnDisable()
+    {
+        EditorApplication.update -= OnUpdate;
+        
+        DoClear();
+
+        draw_gui.DoClear();
+    }
+
+    void DoClear()
+    {
+        db_opt_ani.DoClear();
+        m_entity = null;
+        m_ani = null;
+
+        draw_gui.Reset();
+
+        OnResetMember();
+    }
+
+    void DoReInit()
+    {
+        DoClear();
+
+        DoInit();
+    }
+
+    void DoInit()
+    {
+        db_opt_time.DoReInit(isUpAniByEdTime);
+        OnInitAni();
+        OnResetMember();
+    }
+
+    void OnInitAni()
+    {
+        m_entity = target as ED_Skill05;
+        if (m_entity)
+            m_ani = m_entity.GetComponent<Animator>();
+
+        if (m_ani) { 
+            db_opt_ani.DoReInit(m_ani);
+            draw_gui.DoInit(db_opt_ani);
+        }
+    }
+
+    void OnResetMember()
+    {
+        isPauseing = false;
+        isPlaying = false;
+        isUpAniByEdTime = false;
+
+        OnResetMemberReckon();
+    }
+
+    void OnResetMemberReckon()
+    {
+        db_opt_time.OnResetMemberReckon();
+    }
+
+    void DoPause()
+    {
+
+    }
+
+    void DoResume()
+    {
+        db_opt_time.DoResume();
+    }
+
+    void OnUpdate()
+    {
+        if (m_ani == null || isPauseing || !isPlaying || Application.isPlaying || EditorApplication.isPlaying)
+        {
+            return;
+        }
+
+        db_opt_time.DoUpdateTime();
+
+        // db_opt_ani.DoUpdateAnimator(db_opt_time.DeltaTime, cur_speed);
+
+        db_opt_ani.DoUpdateAnimator(db_opt_time.DeltaTime, draw_gui.CurSpeed,
+            delegate () { OnResetMemberReckon(); },
+            delegate (bool isloop)
+            {
+                OnResetMemberReckon();
+                if (!isloop)
+                {
+                    isPlaying = false;
+                }
+            }
+        );
+    }
+
+    void OnInitM_Ani()
+    {
+        db_opt_ani.DoResetAniCtrl();
+        db_opt_ani.OnResetMemberReckon();
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        draw_gui.DrawRowLine();
+
+        if (!draw_gui.DrawJudged())
+        {
+            return;
+        }
+
+        draw_gui.DrawRefreshAnimator();
+
+        draw_gui.DrawAniListIndex();
+
+        draw_gui.DrawAniInfo();
+
+        draw_gui.DrawSpeed();
+
+        EditorGUILayout.BeginHorizontal();
+        {
+            if (GUILayout.Button("Play"))
+            {
+                OnResetMember();
+                OnInitM_Ani();
+                isPlaying = true;
+
+                db_opt_ani.SetCurCondition();
+            }
+
+            if (GUILayout.Button(isPauseing ? "ReGo" : "Pause"))
+            {
+                isPauseing = !isPauseing;
+                if (!isPauseing)
+                {
+                    DoResume();
+                }
+            }
+
+            if (GUILayout.Button("Stop"))
+            {
+                OnInitM_Ani();
+                OnResetMember();
+            }
+        }
+        EditorGUILayout.EndHorizontal();
+    }
+
+    void OnInspectorUpdate()
+    {
+        Debug.Log("面板的更新");
+    }
+
+    void OnGUI()
+    {
+        Debug.Log("面板的更新 OnGUI");
+    }
+}
