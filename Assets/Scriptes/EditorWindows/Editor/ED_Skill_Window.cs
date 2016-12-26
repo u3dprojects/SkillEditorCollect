5bdf0527b958d9b484e416692ddc4194e875cdc0
Canyon <379657505@qq.com>
2016-12-26 18:29:49 +0800

    添加编辑器window窗口

---
+++ Assets/Scriptes/EditorWindows/Editor/ED_Skill_Window.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class ED_Skill_Window : EditorWindow
+{
+    static bool isOpenWindowView = false;
+
+    static ED_Skill_Window vwWindow = null;
+
+    [MenuItem("Tools/Edtior_Skill")]
+    static void AddWindow()
+    {
+        if (isOpenWindowView)
+            return;
+
+        isOpenWindowView = true;
+        vwWindow = GetWindow<ED_Skill_Window>("技能编辑器");
+    }
+
+    DBOpt_Ani m_db_ani = new DBOpt_Ani();
+
+    DBOpt_GUI m_gui_draw = new DBOpt_GUI();
+
+    DBOpt_Time m_db_time = new DBOpt_Time();
+
+    void OnGUI()
+    {
+
+    }
+}
