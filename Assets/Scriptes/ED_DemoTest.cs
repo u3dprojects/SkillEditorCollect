ac3aeb3516e4cb85729f7efd1443c3610dd84871
Canyon <379657505@qq.com>
2016-12-28 09:30:59 +0800

    添加测试脚本

---
+++ Assets/Scriptes/ED_DemoTest.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class ED_DemoTest : MonoBehaviour {
+
+    DBU3D_AniEffect effect = new DBU3D_AniEffect();
+
+	// Use this for initialization
+	void Start () {
+        effect.DoReInit(this.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
