b2b6b61a9147ec2b09f56002d15736b77098bbad
Canyon <379657505@qq.com>
2016-12-28 11:17:25 +0800

    抽象时间管理，和粒子特效管理

--- Assets/Scriptes/Manager/ED_Particle_Manager.cs
+++ Assets/Scriptes/Manager/ED_Particle_Manager.cs
@@ -1,12 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 /// <summary>
 /// 类名 : 粒子特效管理
 /// 作者 : Canyon
 /// 日期 : 2016-12-27 10:10
 /// 功能 : 
 /// </summary>
+[ExecuteInEditMode]
 public class ED_Particle_Manager : MonoBehaviour {
 
     private static ED_Particle_Manager _m_instance;
@@ -17,10 +22,10 @@ public class ED_Particle_Manager : MonoBehaviour {
         {
             if (_m_instance == null)
             {
-                GameObject gobj = GameObject.Find("ParticleManager");
+                GameObject gobj = GameObject.Find("EDParticleManager");
                 if (gobj == null)
                 {
-                    gobj = new GameObject("ParticleManager");
+                    gobj = new GameObject("EDParticleManager");
                 }
 
                 _m_instance = gobj.GetComponent<ED_Particle_Manager>();
@@ -37,6 +42,7 @@ public class ED_Particle_Manager : MonoBehaviour {
     List<DBU3D_Particle> rmList = new List<DBU3D_Particle>();
     DBU3D_Particle tmp;
     int lens = 0;
+    bool isPause = false;
 
     public void DoInit()
     {
@@ -50,37 +56,45 @@ public class ED_Particle_Manager : MonoBehaviour {
         db.Simulate(0);
     }
 
+    public void OnUpdate()
+    {
+        OnUpdate(ED_Time_Manager.m_instance.DeltaTime);
+    }
+
     public void OnUpdate(float deltatime)
     {
         rmList.Clear();
+        if (isPause)
+            return;
 
+        lens = list.Count;
+        for (int i = 0; i < lens; i++)
+        {
+            tmp = list[i];
+            tmp.Simulate(deltatime);
+        }
+        OnClearParticle();
+    }
+
+    void OnClearParticle(bool isAll = false)
+    {
         lens = list.Count;
 
         for (int i = 0; i < lens; i++)
         {
             tmp = list[i];
-            tmp.Simulate(deltatime);
-            if (tmp.isEndLife)
+            if (isAll)
             {
                 rmList.Add(tmp);
-            }
-        }
-
-        lens = rmList.Count;
-        for (int i = 0; i < lens; i++)
-        {
-            tmp = rmList[i];
-            if (tmp.isEndLife)
+            }else
             {
-                tmp.DoDestory();
-                list.Remove(tmp);
+                if (tmp.isEndLife)
+                {
+                    rmList.Add(tmp);
+                }
             }
         }
-        tmp = null;
-    }
 
-    void DoClear()
-    {
         lens = rmList.Count;
         for (int i = 0; i < lens; i++)
         {
@@ -88,19 +102,41 @@ public class ED_Particle_Manager : MonoBehaviour {
             tmp.DoDestory();
             list.Remove(tmp);
         }
+        tmp = null;
+    }
 
-        lens = list.Count;
-
-        for (int i = 0; i < lens; i++)
-        {
-            tmp = list[i];
-            tmp.DoDestory();
-        }
+    void DoClear()
+    {
+        OnClearParticle(true);
 
         list.Clear();
         rmList.Clear();
         tmp = null;
+        isPause = false;
     }
+
+    public void DoPause()
+    {
+        isPause = true;
+    }
+
+    public void DoResume()
+    {
+        isPause = false;
+    }
+
+#if UNITY_EDITOR
+    void OnEnable()
+    {
+        EditorApplication.update += Update;
+    }
+
+    void OnDisable()
+    {
+        DoClear();
+        EditorApplication.update -= Update;
+    }
+#endif
 
     // Use this for initialization
     void Start () {
@@ -109,6 +145,6 @@ public class ED_Particle_Manager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        OnUpdate();
+    }
 }
---
+++ Assets/Scriptes/Manager/ED_Time_Manager.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+/// <summary>
+/// 类名 : 时间管理
+/// 作者 : Canyon
+/// 日期 : 2016-12-28 09:20
+/// 功能 : 
+/// </summary>
+[ExecuteInEditMode]
+public class ED_Time_Manager : MonoBehaviour {
+
+    private static ED_Time_Manager _m_instance;
+
+    static public ED_Time_Manager m_instance
+    {
+        get
+        {
+            if (_m_instance == null)
+            {
+                GameObject gobj = GameObject.Find("EDTimeManager");
+                if (gobj == null)
+                {
+                    gobj = new GameObject("EDTimeManager");
+                }
+
+                _m_instance = gobj.GetComponent<ED_Time_Manager>();
+                if (_m_instance == null)
+                {
+                    _m_instance = gobj.AddComponent<ED_Time_Manager>();
+                }
+            }
+            return _m_instance;
+        }
+    }
+
+    DBOpt_Time m_time = new DBOpt_Time();
+
+#if UNITY_EDITOR
+    void OnEnable()
+    {
+        EditorApplication.update += Update;
+    }
+
+    void OnDisable()
+    {
+        EditorApplication.update -= Update;
+    }
+#endif
+
+    public void DoPause()
+    {
+        m_time.DoPause();
+    }
+
+    public void DoResume()
+    {
+        m_time.DoResume();
+    }
+
+    public void DoInit()
+    {
+        m_time.DoReInit(false);
+    }
+
+    public void DoReset()
+    {
+        m_time.OnResetMemberReckon();
+    }
+
+    public float DeltaTime
+    {
+        get
+        {
+            return m_time.DeltaTime;
+        }
+    }
+
+    public float ProgressTime
+    {
+        get
+        {
+            return m_time.ProgressTime;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        m_time.DoUpdateTime();
+    }
+}
