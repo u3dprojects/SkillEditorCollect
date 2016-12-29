28d4f1eef1c851be4f549e9cf2d9e5c593998b24
Canyon <379657505@qq.com>
2016-12-29 17:24:42 +0800

    添加雨神游戏扩展开发脚本

---
+++ Assets/Scriptes/DBOpt/YGame/ED_Ani_YGame.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 类名 : 融合项目-动作Animator
+/// 作者 : Canyon
+/// 日期 : 2016-12-29 17:10:00
+/// 功能 : 
+/// </summary>
+[System.Serializable]
+public class ED_Ani_YGame : ED_Ani {
+
+}
