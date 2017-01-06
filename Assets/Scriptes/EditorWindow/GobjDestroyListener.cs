1b811318ace857ef4abc5235c6b5a8cc537a1de6
Canyon <379657505@qq.com>
2017-01-06 18:14:57 +0800

    添加测试脚本

---
+++ Assets/Scriptes/EditorWindow/GobjDestroyListener.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 类名 : 对象销毁时候调用回调函数
+/// 作者 : Canyon
+/// 日期 : 2017-01-06 11:10
+/// 功能 : 
+/// </summary>
+public class GobjDestroyListion : MonoBehaviour {
+
+    System.Action Call4Destroy;
+    
+    protected virtual void OnDestroy()
+    {
+        Debug.Log("== OnDestroy ==");
+        if (Call4Destroy != null)
+        {
+            Call4Destroy();
+            this.Call4Destroy = null;
+        }
+    }
+
+    public void AddCall4Destroy(System.Action funcCall)
+    {
+        if(this.Call4Destroy == null)
+        {
+            this.Call4Destroy = funcCall;
+        }else
+        {
+            this.Call4Destroy += funcCall;
+        }
+    }
+}
---
+++ Assets/Scriptes/_Demo/RD_DT1.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 类名 : R-Runnin,D-Demo，T-Test 运行模式下的代码测试类
+/// 作者 : Canyon
+/// 日期 : 2017-01-06 17:10
+/// 功能 : 
+/// </summary>
+public class RD_DT1 : MonoBehaviour {
+
+    public bool isOpen = true;
+    float timeOut = 3;
+    float timeCur = 0;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(Test());
+        timeOut = 3;
+        timeCur = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (timeCur >= timeOut)
+        {
+            // 当隐藏时，该脚本开启的协程会主动关闭
+            // gameObject.SetActive(false);
+        }
+
+        timeCur += Time.deltaTime;
+    }
+
+    IEnumerator Test()
+    {
+        while (isOpen)
+        {
+            Debug.Log("RD_DT1 Test");
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+}
