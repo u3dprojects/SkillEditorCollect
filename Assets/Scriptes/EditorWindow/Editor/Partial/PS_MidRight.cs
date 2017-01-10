b5f99a2f6fb1cd41997c1d31d0e3c4ab03a06fae
Canyon <379657505@qq.com>
2017-01-10 18:18:50 +0800

    绘制Skill表的属性,进行数据读取，和保存

--- Assets/Scriptes/EditorWindow/Editor/Partial/PS_MidRight.cs
+++ Assets/Scriptes/EditorWindow/Editor/Partial/PS_MidRight.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEditor;
+using System.IO;
 
 /// <summary>
 /// 类名 : P-Partial,S-Skill 策划工具:技能数据表
@@ -22,9 +23,49 @@ public class PS_MidRight{
         }
     }
 
-    bool isEmptySkillPath = true;
+    EN_SkillOpt optSkill
+    {
+        get { return EN_SkillOpt.Instance; }
+    }
+
+    Vector2 scrollPos;
+    float minScrollH = 260;
+    float curScrollH = 260;
+    float minWidth = 440;
+    float curWidth = 0;
+
+    // 技能
     string pathOpenSkill = "";
-    
+    string prePathOpenSkill = "";
+
+    // 技能属性
+    EN_Skill ms_curSkill;
+
+    int ms_ID;
+    string ms_name;
+
+    int ms_actId;
+
+    string[] CastTypes = { "默认技能","打断式技能","位移控制技能"};
+    int ms_castType;
+
+    string[] ElementType = {"物理","冰","火"};
+    int ms_elementType;
+
+    float ms_elmDamageRate;
+
+    int ms_belongType;
+    string[] BelongType = {"玩家","雇佣兵","怪物"};
+
+    int ms_slotIdx_Int;
+
+    int ms_lockType;
+    string[] LockType = { "无锁定", "锁定", "锁定怪物(预留暂无用)" };
+
+    float maxDistance, minDistance,cooldown,duration,beforeRoll,afterRoll;
+
+    string evtStr;
+
     #endregion
 
     public void DoInit(EDW_Skill org)
@@ -32,18 +73,40 @@ public class PS_MidRight{
         this.m_wSkill = org;
     }
     
+    void RecokenWH()
+    {
+        if (this.m_wSkill != null)
+        {
+            // 100 - 是主界面顶部高度 20 - 是误差偏移
+            curScrollH = (m_wSkill.position.height - 100) - 20;
+            curScrollH = Mathf.Max(curScrollH, minScrollH);
+
+            curWidth = (m_wSkill.position.width - 10) / 2;
+            curWidth = Mathf.Max(curWidth, minWidth);
+        }
+    }
+
     public void DrawShow()
     {
-        EG_GUIHelper.FEG_BeginVArea();
+        RecokenWH();
+
+        EG_GUIHelper.FEG_BeginVArea(curWidth);
         {
             _DrawDesc();
-
-            _DrawSearchSkillExcel();
-
+            
             if (this.m_wSkill.gobjEntity)
             {
-                
-                
+                EG_GUIHelper.FEG_BeginScroll(ref scrollPos, 0, 0, curScrollH);
+
+                if (this.m_wSkill._DrawAniJudged()) { 
+                    _DrawSearchSkillExcel();
+
+                    _DrawSkill();
+
+                    _DrawOptBtns();
+                }
+
+                EG_GUIHelper.FEG_EndScroll();
             }
         }
         EG_GUIHelper.FEG_EndV();
@@ -64,83 +127,216 @@ public class PS_MidRight{
     void _DrawSearchSkillExcel()
     {
         EG_GUIHelper.FEG_BeginH();
-        isEmptySkillPath = string.IsNullOrEmpty(pathOpenSkill);
-        EG_GUIHelper.FEG_BeginToggleGroup("选取技能表excel文件", ref isEmptySkillPath);
-        if (GUILayout.Button("选取SkillExcel"))
+        if (GUILayout.Button("选取Excel(Skill)"))
         {
-            pathOpenSkill = UnityEditor.EditorUtility.OpenFilePanel("选取excel文件", "", "xls");
-        }
-        EG_GUIHelper.FG_Space(3);
-        EG_GUIHelper.FEG_EndToggleGroup();
-        
-        EG_GUIHelper.FEG_EndH();
-    }
-
-    bool _DrawAniJudged()
-    {
-        bool ret = false;
-        EG_GUIHelper.FEG_BeginH();
-        {
-            EG_GUIHelper.FEG_BeginV();
-            {
-                if (m_wSkill == null || m_curAni == null)
-                {
-                    EditorGUILayout.HelpBox("请拖动Prefab到Model Prefab中！", MessageType.Error);
-                }
-                else if (!m_curAni.IsHasAniCtrl)
-                {
-                    EditorGUILayout.HelpBox("该Animator里面没有AnimatorController \n\n请添加动画控制器-AnimatorController！", MessageType.Error);
-                }
-                else if (m_curAni.Keys.Count <= 0)
-                {
-                    EditorGUILayout.HelpBox("该AnimatorController里面没有任何动画，请添加State动画！", MessageType.Error);
-                }
-                else
-                {
-                    ret = true;
-                }
-            }
-            EG_GUIHelper.FEG_EndV();
+            this.pathOpenSkill = UnityEditor.EditorUtility.OpenFilePanel("选取excel文件", "", "xls");
         }
         EG_GUIHelper.FEG_EndH();
-        return ret;
-    }
 
-    void _DrawAniList()
-    {
-        EG_GUIHelper.FEG_BeginH();
-        GUIStyle style = EditorStyles.label;
-        style.alignment = TextAnchor.MiddleLeft;
-        EditorGUILayout.LabelField("动画列表:", style);
-        EG_GUIHelper.FEG_EndH();
-    }
+        EG_GUIHelper.FG_Space(5);
 
-    void _DrawAniStateInfo()
-    {
-        EG_GUIHelper.FEG_BeginH();
-        GUIStyle style = EditorStyles.label;
-        style.alignment = TextAnchor.MiddleLeft;
-
-        GUILayoutOption minW = GUILayout.MinWidth(90);
-        GUILayout.Label("总帧数: " + m_curAni.CurFrameCount, style, minW);
-
-        GUILayout.Label("总时长: " + m_curAni.CurLens + " s", style, minW);
-
-        style.alignment = TextAnchor.MiddleRight;
-        EditorGUILayout.LabelField("动画帧率: " + m_curAni.CurFrameRate + " 帧/s", style);
-        EG_GUIHelper.FEG_EndH();
-    }
-
-    void _DrawAniMinMaxSpeed()
-    {
-        EG_GUIHelper.FEG_BeginH();
+        if (string.IsNullOrEmpty(this.pathOpenSkill))
         {
-            bool v = false;
-            EG_GUIHelper.FEG_BeginToggleGroup("MinMax速度??", ref v);
-
-            EG_GUIHelper.FEG_EndToggleGroup();
+            EG_GUIHelper.FEG_BeginH();
+            EditorGUILayout.HelpBox("请单击选取Excel,目前不能根据ID进行查询?", MessageType.Error);
+            EG_GUIHelper.FEG_EndH();
+        }else
+        {
+            EG_GUIHelper.FEG_BeginH();
+            EG_GUIHelper.FG_Label("SkillPath:" + this.pathOpenSkill);
+            EG_GUIHelper.FEG_EndH();
         }
-        EG_GUIHelper.FEG_EndH();
+
+        EG_GUIHelper.FG_Space(5);
+
+        if (!this.prePathOpenSkill.Equals(this.pathOpenSkill))
+        {
+            optSkill.DoInit(this.pathOpenSkill, 0);
+            this.prePathOpenSkill = this.pathOpenSkill;
+        }
     }
+
     
+    void _DrawSkill()
+    {
+        EG_GUIHelper.FEG_BeginH();
+        ms_ID = EditorGUILayout.IntField("技能ID:",ms_ID);
+        if (GUILayout.Button("SeachSkill"))
+        {
+            if (optSkill.isInitSuccessed)
+            {
+                ms_curSkill = optSkill.GetEnSkill(ms_ID);
+                OnInitEntity2Attrs();
+            }
+            else{
+                EditorUtility.DisplayDialog("Tips","没有选则技能表，不能进行查询搜索!","Okey");
+            }
+        }
+        EG_GUIHelper.FEG_EndH();
+        EG_GUIHelper.FG_Space(5);
+
+        EG_GUIHelper.FEG_BeginH();
+        ms_name = EditorGUILayout.TextField("技能名称:", ms_name);
+        EG_GUIHelper.FEG_EndH();
+        EG_GUIHelper.FG_Space(5);
+
+        EG_GUIHelper.FEG_BeginH();
+        ms_actId = EditorGUILayout.IntField("SkillIndex:", ms_actId);
+
+        GUI.color = Color.yellow;
+        EG_GUIHelper.FG_Label("触发该节能的条件,比如该技能是Animator\nController里面的Skill1,此地就填写数字:1");
+        GUI.color = Color.white;
+        EG_GUIHelper.FEG_EndH();
+        EG_GUIHelper.FG_Space(5);
+
+        EG_GUIHelper.FEG_BeginH();
+        ms_castType = EditorGUILayout.Popup("释放类型:", ms_castType, CastTypes);
+        EG_GUIHelper.FEG_EndH();
+        EG_GUIHelper.FG_Space(5);
+
+        EG_GUIHelper.FEG_BeginH();
+        ms_elementType = EditorGUILayout.Popup("元素类型:", ms_elementType, ElementType);
+        EG_GUIHelper.FEG_EndH();
+        EG_GUIHelper.FG_Space(5);
+
+        EG_GUIHelper.FEG_BeginH();
+        ms_elmDamageRate = EditorGUILayout.FloatField("元素伤害系数:", ms_elmDamageRate);
+        EG_GUIHelper.FEG_EndH();
+        EG_GUIHelper.FG_Space(5);
+
+        EG_GUIHelper.FEG_BeginH();
+        ms_belongType = EditorGUILayout.Popup("所属对象类型:", ms_belongType, BelongType);
+        EG_GUIHelper.FEG_EndH();
+        EG_GUIHelper.FG_Space(5);
+
+        EG_GUIHelper.FEG_BeginH();
+        ms_slotIdx_Int = EditorGUILayout.IntField("技能槽位:", ms_slotIdx_Int);
+        EG_GUIHelper.FEG_EndH();
+        EG_GUIHelper.FG_Space(5);
+
+        EG_GUIHelper.FEG_BeginH();
+        ms_lockType = EditorGUILayout.Popup("锁定类型:", ms_lockType, LockType);
+        EG_GUIHelper.FEG_EndH();
+        EG_GUIHelper.FG_Space(5);
+
+        EG_GUIHelper.FEG_BeginH();
+        maxDistance = EditorGUILayout.FloatField("最大释放距离(单位米):", maxDistance);
+        EG_GUIHelper.FEG_EndH();
+        EG_GUIHelper.FG_Space(5);
+
+        EG_GUIHelper.FEG_BeginH();
+        minDistance = EditorGUILayout.FloatField("最小释放距离(单位米):", minDistance);
+        EG_GUIHelper.FEG_EndH();
+        EG_GUIHelper.FG_Space(5);
+
+        EG_GUIHelper.FEG_BeginH();
+        cooldown = EditorGUILayout.FloatField("冷却时间:", cooldown);
+        EG_GUIHelper.FEG_EndH();
+        EG_GUIHelper.FG_Space(5);
+
+        EG_GUIHelper.FEG_BeginH();
+        duration = EditorGUILayout.FloatField("技能时长(单位秒):", duration);
+        EG_GUIHelper.FEG_EndH();
+        EG_GUIHelper.FG_Space(5);
+
+        EG_GUIHelper.FEG_BeginH();
+        beforeRoll = EditorGUILayout.FloatField("前摇结束点(单位秒):", beforeRoll);
+        EG_GUIHelper.FEG_EndH();
+        EG_GUIHelper.FG_Space(5);
+
+        EG_GUIHelper.FEG_BeginH();
+        afterRoll = EditorGUILayout.FloatField("后摇开始点(单位秒):", afterRoll);
+        EG_GUIHelper.FEG_EndH();
+        EG_GUIHelper.FG_Space(5);
+
+        // 绘制技能事件
+        EG_GUIHelper.FG_BeginVAsArea();
+        {
+            EG_GUIHelper.FEG_BeginH();
+            EditorGUILayout.LabelField("技能事件:",evtStr, EditorStyles.textArea);
+            EG_GUIHelper.FEG_EndH();
+        }
+        EG_GUIHelper.FG_EndV();
+    }
+
+    void _DrawOptBtns()
+    {
+        EG_GUIHelper.FEG_BeginH();
+        {
+            if (GUILayout.Button("Save Excel"))
+            {
+                if (!optSkill.isInitSuccessed)
+                {
+                    EditorUtility.DisplayDialog("Tips", "没有选则技能表，不能进行保存!", "Okey");
+                    return;
+                }
+
+                string folder = Path.GetDirectoryName(this.pathOpenSkill);
+                string fileName = Path.GetFileNameWithoutExtension(this.pathOpenSkill);
+                string suffix = Path.GetExtension(this.pathOpenSkill);
+                suffix = suffix.Replace(".", "");
+                string savePath = UnityEditor.EditorUtility.SaveFilePanel("保存Excel文件", folder, fileName, suffix);
+
+                if (string.IsNullOrEmpty(savePath))
+                {
+                    UnityEditor.EditorUtility.DisplayDialog("Tips", "The save path is Empty !!!", "Okey");
+                    return;
+                }
+
+                OnInitAttrs2Entity();
+
+                optSkill.Save(savePath);
+            }
+        }
+        EG_GUIHelper.FEG_EndH();
+    }
+
+    void OnInitEntity2Attrs()
+    {
+        if(ms_curSkill != null)
+        {
+            this.ms_ID = this.ms_curSkill.c00_ID;
+            this.ms_name = this.ms_curSkill.c01_Name;
+            this.ms_actId = this.ms_curSkill.c02_ActId;
+            this.ms_castType = this.ms_curSkill.c03_CastType_Int;
+            this.ms_elementType = this.ms_curSkill.c04_ElementType_Int;
+            this.ms_elmDamageRate = this.ms_curSkill.c05_DmgAdditional;
+            this.ms_belongType = this.ms_curSkill.c06_SlotObjTp_Int;
+            this.ms_slotIdx_Int = this.ms_curSkill.c07_SlotIdx_Int;
+            this.ms_lockType = this.ms_curSkill.c08_LockTp_Int;
+            this.maxDistance = this.ms_curSkill.c09_CastDistFarthest;
+            this.minDistance = this.ms_curSkill.c10_CastDistNearest;
+            this.cooldown = this.ms_curSkill.c11_CD;
+            this.duration = this.ms_curSkill.c12_Duration;
+            this.beforeRoll = this.ms_curSkill.c14_PreCastTiming;
+            this.afterRoll = this.ms_curSkill.c15_PostCastTiming;
+
+            // 事件处理有点麻烦，单独出来写
+            this.evtStr = this.ms_curSkill.c13_CastEvent_Str;
+        }
+    }
+
+    void OnInitAttrs2Entity()
+    {
+        ms_curSkill = optSkill.GetOrNew(ms_ID);
+
+        this.ms_curSkill.c00_ID = this.ms_ID;
+        this.ms_curSkill.c01_Name = this.ms_name;
+        this.ms_curSkill.c02_ActId = this.ms_actId;
+        this.ms_curSkill.c03_CastType_Int = this.ms_castType;
+        this.ms_curSkill.c04_ElementType_Int = this.ms_elementType;
+        this.ms_curSkill.c05_DmgAdditional = this.ms_elmDamageRate;
+        this.ms_curSkill.c06_SlotObjTp_Int = this.ms_belongType;
+        this.ms_curSkill.c07_SlotIdx_Int = this.ms_slotIdx_Int;
+        this.ms_curSkill.c08_LockTp_Int = this.ms_lockType;
+        this.ms_curSkill.c09_CastDistFarthest = this.maxDistance;
+        this.ms_curSkill.c10_CastDistNearest = this.minDistance;
+        this.ms_curSkill.c11_CD = this.cooldown;
+        this.ms_curSkill.c12_Duration = this.duration;
+        this.ms_curSkill.c14_PreCastTiming = this.beforeRoll;
+        this.ms_curSkill.c15_PostCastTiming = this.afterRoll;
+
+        this.ms_curSkill.c13_CastEvent_Str = this.evtStr;
+    }
+
 }
